306da7179906221914c38794f8339bf54f884578
myidrajkumar <myidrajkumar@gmail.com>
2018-12-31 23:01:18 +0530

    LearningReactWithRedux

---
+++ tools/startMessage.js
@@ -0,0 +1,4 @@
+import colors from 'colors';
+
+/* eslint-disable no-console */
+console.log('Hello Ekaa, I started learning react and redux'.green);
---
+++ webpack.config.dev.js
@@ -0,0 +1,40 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+    default: true,
+    devtool: 'inline-source-map',
+    noInfo: false,
+    entry: [
+        'eventsource-polyfill',  //Hot reloading in IE will happen onlf if this is included
+        'webpack-hot-middleware/client?reload=true', //To hot reload
+        './src/index'
+    ],
+    target: 'web',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './src'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+            {test: /(\.css)$/, loaders: ['style', 'css']},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        ]
+    },
+    config: {
+        optimization: {
+          noEmitOnErrors: true
+        }
+    }
+};
